41bdb0a8d954ece4ebdd57895f5d72c1fc532301
akamensi <akez.muhammed@gmail.com>
2024-07-06 15:32:24 +0200

    Hover Events

--- src/Components/StarRating.js
+++ src/Components/StarRating.js
@@ -18,7 +18,8 @@ const textStyle = {
 };
 
 const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -30,11 +31,13 @@ const StarRating = ({ maxRating = 5 }) => {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 };
