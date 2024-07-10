def58c6e74324adc29b053131a1726da9636365f
akamensi <akez.muhammed@gmail.com>
2024-07-11 00:32:25 +0200

    useRef to select DOM elements

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,27 @@
+import { useEffect, useRef } from "react";
+
 const Search = ({ query, setQuery }) => {
+  //This not The REACT way it's better to use useRef Hook
+  /*   useEffect(() => {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  }, []); */
+
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callBack = (e) => {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+    document.addEventListener("keydown", callBack);
+    return () => document.addEventListener("keydown", callBack);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +29,7 @@ const Search = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
